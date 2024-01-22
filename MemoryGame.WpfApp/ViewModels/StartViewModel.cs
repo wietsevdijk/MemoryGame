9555807e099ca67e622922111328affa9f9179bf
Wietse <wietsedaanvandijk@hotmail.com>
2024-01-22 18:37:16 +0100

    begin UI

---
+++ MemoryGame.WpfApp/ViewModels/StartViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MemoryGame.WpfApp.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MemoryGame.WpfApp.ViewModels {
+    public partial class StartViewModel : ObservableObject {
+
+        [ObservableProperty]
+        private string _playerName = "";
+
+        [ObservableProperty]
+        private int _cardPairCount;
+
+        public StartViewModel() {
+            StartGameCommand = new RelayCommand(StartGame);
+        }
+
+        public ICommand StartGameCommand { get; }
+
+        private void StartGame() {
+            MessageBox.Show("Kaas");
+        }
+
+    }
+}
