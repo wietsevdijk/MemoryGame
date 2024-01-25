31164c11be8a46ddabb83178742752357d5f995e
Wietse <wietsedaanvandijk@hotmail.com>
2024-01-25 20:35:40 +0100

    Create PathToBitmapImageConverter.cs

---
+++ MemoryGame.WpfApp/PathToBitmapImageConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace MemoryGame.WpfApp {
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    public class PathToBitmapImageConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            string filePath = (string)value;
+            
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.UriSource = new Uri(filePath, UriKind.Relative);
+            image.EndInit();
+
+            return image;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
