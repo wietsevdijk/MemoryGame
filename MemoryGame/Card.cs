9462e9d682dbb90e21fc7c88b6ea735511c04012
Wietse <wietsedaanvandijk@hotmail.com>
2024-01-28 18:01:36 +0100

    Foto's met nummer als fallback

--- MemoryGame/Card.cs
+++ MemoryGame/Card.cs
@@ -5,33 +5,36 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-namespace MemoryGame
-{
-    public class Card
-    {
+namespace MemoryGame {
+    public class Card {
         private int _position;
         public int Position { get => _position; set => _position = value; }
         private int _value;
         public int Value { get => _value; set => _value = value; }
         private bool _discovered;
         public bool Discovered { get => _discovered; set => _discovered = value; }
-        private string _backsideImage = GlobalConfig.ImageFilePath + "achterkant.jpg";
+        private string _backsideImage = GlobalConfig.ImageFilePath + "asdachterkant.jpg";
         public string BacksideImage { get => _backsideImage; set => _backsideImage = value; }
-        private string _frontsideImage = GlobalConfig.ImageFilePath + "voorkant.jpg";
+        private string _frontsideImage = GlobalConfig.ImageFilePath + "dsavoorkant.jpg";
         public string FrontsideImage { get => _frontsideImage; set => _frontsideImage = value; }
 
         // NOTE: All properties below are only used for the GUI
         private bool _flipped;
         public bool Flipped { get => _flipped; set => _flipped = value; }
-        public bool CardAvailable => !_discovered;
-        public string VisibleValue => _flipped ? _value.ToString() : string.Empty;
-        public string VisibleImage => _flipped ? _frontsideImage : _backsideImage;
+        public bool CardAvailable => CheckCardAvailable();
 
         public Card(int id, int inputValue) { 
             _position = id;
             _value = inputValue;
             _flipped = false;
             _discovered = false;
+        }
+
+        private bool CheckCardAvailable() {
+            if (_discovered) {
+                return false;
+            }
+            return !_flipped;
         }
 
     }
